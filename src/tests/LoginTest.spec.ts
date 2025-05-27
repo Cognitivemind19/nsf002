9e503e0be492c4f1f24a57217a0c757147770046
cognitivemind19 <bipin.cognitivemind@gmail.com>
2025-05-27 19:10:34 +0530

    updated encrption method

--- src/tests/LoginTest.spec.ts
+++ src/tests/LoginTest.spec.ts
@@ -4,7 +4,7 @@ import HomePage from "../pages/HomePage";
 import { encrypt, decrypt } from "../utils/cryptoUtil";
 import { encryptEnvFile, decryptEnvFile } from "../utils/encryptEnvFile";
 
-test("Login Test", async ({ page }) => {
+test.skip("Login Test", async ({ page }) => {
   const loginPage = new LoginPage(page);
   console.log(process.env.USER_ID!);
   console.log(process.env.PASSWORD!);
@@ -16,12 +16,12 @@ test("Login Test", async ({ page }) => {
   await homePage.expectServiceTitleToBeVisible();
 });
 
-// test("Sample Test", async ({ page }) => {
-//   // const plainText = "Bipin Behera";
-//   // const encryptedText = encrypt(plainText);
-//   // console.log("SALT", process.env.SALT);
-//   // console.log("Encrypted Text:", encryptedText);
-//   // const decryptedText = decrypt(encryptedText);
-//   // console.log("Decrypted Text:", decryptedText);
-//   encryptEnvFile();
-// });
+test("Sample Test", async ({ page }) => {
+  // const plainText = "Bipin Behera";
+  // const encryptedText = encrypt(plainText);
+  // console.log("SALT", process.env.SALT);
+  // console.log("Encrypted Text:", encryptedText);
+  // const decryptedText = decrypt(encryptedText);
+  // console.log("Decrypted Text:", decryptedText);
+  encryptEnvFile();
+});
